8233be903d5f1d329f6a310e2728e802016869b5
Kaamkiya <kaamkiya@github.com>
2024-01-11 09:50:23 -0500

    Update install script

--- init.sh
+++ init.sh
@@ -1,6 +1,11 @@
 echo "\033[38;5;129mDevl Installation\033[0m"
 
-git clone https://github.com/Kaamkiya/cli-learn.git ~/.devl -b stable
+git clone https://github.com/Kaamkiya/devl.git ~/.devl
 cd ~/.devl
 mkdir bin
-go build -o bin/devl *.go
+go build -o devl *.go
+
+export PATH="$PATH:~/.devl"
+
+echo 'To permanently add Devl to your path, add the following to your ~/.profile':
+echo '    export PATH="$PATH:~/.devl"'
