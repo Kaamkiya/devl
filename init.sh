b3014f9b97d4b511eb8cb408004481893d45d477
Kaamkiya <138614660+Kaamkiya@users.noreply.github.com>
2024-01-14 11:40:23 -0500

    (feat/fix):Update install script
    + Add ANSI colors to help users understand the output
    + Fix: clone repo to ~/.devl, and then build from there, instead of
      building from $CWD.

--- init.sh
+++ init.sh
@@ -1,7 +1,12 @@
-echo "\033[38;5;129mDevl Installation\033[0m"
+echo "\033[38;5;129;1mDevl Installation\033[0m"
 
+echo "\033[32;1mCloning the repo...\033[38;2;170;170;180;22m"
 git clone https://github.com/Kaamkiya/devl.git ~/.devl
-go build -o devl *.go
+
+echo "\033[32;1mBuilding from source...\033[0m"
+go build -o ~/.devl/devl ~/.devl/*.go
+
+echo "Done."
 
 export PATH="$PATH:~/.devl"
 
